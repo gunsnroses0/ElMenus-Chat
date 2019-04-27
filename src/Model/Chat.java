f71339c9e3042fda12d20f167e8134c409af6f07
ahmedash11 <ahmedashh11@gmail.com>
2019-04-27 19:12:17 +0200

    Update Chat.java

--- src/Model/Chat.java
+++ src/Model/Chat.java
@@ -23,6 +23,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
 
